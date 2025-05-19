7d6050f16ce64312e1352fd2ab1abc77d8d3534b
Michał Kozłowski <michal.kozlowski032@gmail.com>
2025-05-20 00:50:12 +0200

    feat: make bottom nav visible after auth

--- app/src/main/java/com/example/gym_bro_mobile/fragments/ExercisesFragment.java
+++ app/src/main/java/com/example/gym_bro_mobile/fragments/ExercisesFragment.java
@@ -1,6 +1,33 @@
 package com.example.gym_bro_mobile.fragments;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class MainFragment extends Fragment {
+import com.example.gym_bro_mobile.R;
+import com.example.gym_bro_mobile.databinding.FragmentExercisesBinding;
+
+public class ExercisesFragment extends Fragment {
+    private FragmentExercisesBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        binding = FragmentExercisesBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        requireActivity().findViewById(R.id.bottom_navigation).setVisibility(View.VISIBLE);
+    }
 }
