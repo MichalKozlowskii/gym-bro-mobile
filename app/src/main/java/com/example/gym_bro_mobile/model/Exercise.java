22428d54d724fdfc118284dbeabae30c31bd2703
Michał Kozłowski <michal.kozlowski032@gmail.com>
2025-05-20 00:49:56 +0200

    feat: exercise pojo

---
+++ app/src/main/java/com/example/gym_bro_mobile/model/Exercise.java
@@ -0,0 +1,52 @@
+package com.example.gym_bro_mobile.model;
+
+import java.time.LocalDateTime;
+
+public class Exercise {
+    private Integer id;
+    private String name;
+    private String demonstrationUrl;
+    private LocalDateTime creationDate;
+
+    public Exercise(Integer id, String name, String demonstrationUrl, LocalDateTime creationDate) {
+        this.id = id;
+        this.name = name;
+        this.demonstrationUrl = demonstrationUrl;
+        this.creationDate = creationDate;
+    }
+
+    public Exercise() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDemonstrationUrl() {
+        return demonstrationUrl;
+    }
+
+    public void setDemonstrationUrl(String demonstrationUrl) {
+        this.demonstrationUrl = demonstrationUrl;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+}
